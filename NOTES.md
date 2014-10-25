02f81c939c6151471719aae57d2754f62bb52231
Alexander Gugel <alexander.gugel@gmail.com>
2014-10-25 16:43:50 +0200

    Add popups

---
+++ NOTES.md
@@ -0,0 +1,125 @@
+var sports = {
+  1: {
+    name: 'Archery'
+  },
+  2: {
+    name: 'Athletics'
+  },
+  3: {
+    name: 'Badminton'
+  },
+  4: {
+    name: 'Basketball'
+  },
+  5: {
+    name: 'Beach Volleyball'
+  },
+  6: {
+    name: 'Boxing'
+  },
+  7: {
+    name: 'Canoe Slalom'
+  },
+  8: {
+    name: 'Canoe Sprint'
+  },
+  9: {
+    name: 'Cycling BMX'
+  },
+  10: {
+    name: 'Cycling Mountain Bike'
+  },
+  11: {
+    name: 'Cycling Road'
+  },
+  12: {
+    name: 'Cycling Track'
+  },
+  13: {
+    name: 'Diving'
+  },
+  14: {
+    name: 'Equestrian / Dressage'
+  },
+  15: {
+    name: 'Equestrian / Eventing'
+  },
+  16: {
+    name: 'Equestrian / Jumping'
+  },
+  17: {
+    name: 'Fencing'
+  },
+  18: {
+    name: 'Football'
+  },
+  19: {
+    name: 'Golf'
+  },
+  20: {
+    name: 'Gymnastics Artistic'
+  },
+  21: {
+    name: 'Gymnastics Rhythmic'
+  },
+  22: {
+    name: 'Handball'
+  },
+  23: {
+    name: 'Hockey'
+  },
+  24: {
+    name: 'Judo'
+  },
+  25: {
+    name: 'Modern Pentathlon'
+  },
+  26: {
+    name: 'Rowing'
+  },
+  27: {
+    name: 'Rugby'
+  },
+  28: {
+    name: 'Sailing'
+  },
+  29: {
+    name: 'Shooting'
+  },
+  30: {
+    name: 'Swimming'
+  },
+  31: {
+    name: 'Synchronized Swimming'
+  },
+  32: {
+    name: 'Table Tennis'
+  },
+  33: {
+    name: 'Taekwondo'
+  },
+  34: {
+    name: 'Tennis'
+  },
+  35: {
+    name: 'Trampoline'
+  },
+  36: {
+    name: 'Triathlon'
+  },
+  37: {
+    name: 'Volleyball'
+  },
+  38: {
+    name: 'Water Polo'
+  },
+  39: {
+    name: 'Weightlifting'
+  },
+  40: {
+    name: 'Wrestling Freestyle'
+  },
+  41: {
+    name: 'Wrestling Greco-Roman'
+  }
+};
--- js/map.js
+++ js/map.js
@@ -39,17 +39,23 @@ InteractiveVenueMap.prototype._initClusterGroup = function () {
   this.map.addLayer(this.venueClusterGroup);
 };
 
-InteractiveVenueMap.prototype.renderVenues = function (venues) {
-  for (var i = 0; i < 100; i++) {
-    var marker = L.marker(new L.LatLng(i, i), {
+InteractiveVenueMap.prototype._venueToPopup = function (venue) {
+  return venue.description;
+};
+
+InteractiveVenueMap.prototype.addVenues = function (venues) {
+  for (var i = 0; i < venues.length; i++) {
+    var venue = venues[i];
+    var marker = L.marker(new L.LatLng(venue.lat, venue.lng), {
         icon: L.divIcon({
+          popupAnchor: L.point(0, -15),
           iconSize: L.point(30, 30),
           className: 'venue-marker',
           html: '<div class="venue"></div>'
         }),
-        title: i
+        title: venue.name
     });
-    marker.bindPopup(i);
+    marker.bindPopup(this._venueToPopup(venue));
     this.venueClusterGroup.addLayer(marker);
   }
 };
@@ -58,6 +64,23 @@ InteractiveVenueMap.prototype.clearVenues = function () {
   InteractiveVenueMap.clearLayers();
 };
 
+///
+
+var venues = (function generateDummyVenues () {
+  var venues = [];
+
+  for (var i = 0; i < 40; i++) {
+    venues.push({
+      name: 'Golf Course',
+      description: 'Aute incididunt officia magna tempor ad id nulla incididunt Lorem non eiusmod culpa adipisicing voluptate. Laboris veniam duis do sit ea nostrud esse ea irure in cupidatat. Irure irure cillum Lorem ex cillum anim sit exercitation eu laboris culpa proident eiusmod. Sunt proident esse exercitation mollit aliquip culpa ad. Cillum laborum ea dolore est irure laboris sit nostrud reprehenderit eu laboris tempor eiusmod incididunt. Anim mollit excepteur culpa consequat eu proident Lorem eiusmod consequat est. Commodo culpa dolor pariatur culpa proident aliqua exercitation id in commodo ut.',
+      image: 'http://www.funchap.com/wp-content/uploads/2014/03/baby-elephant-and-egrets.jpg',
+      lat: Math.floor(Math.random()*50),
+      lng: Math.floor(Math.random()*50)
+    });
+  }
+
+  return venues;
+})();
 
 var interactiveVenueMap = new InteractiveVenueMap();
-interactiveVenueMap.renderVenues();
+interactiveVenueMap.addVenues(venues);
