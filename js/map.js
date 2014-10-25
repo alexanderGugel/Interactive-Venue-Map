1b48598c28e06cae1b66d06ea4fc4238fb783fd6
Alexander Gugel <alexander.gugel@gmail.com>
2014-10-25 15:07:37 +0200

    Refactor

--- js/map.js
+++ js/map.js
@@ -1,43 +1,63 @@
 L.mapbox.accessToken = 'pk.eyJ1IjoiYWxleGFuZGVyZ3VnZWwiLCJhIjoiTHF6V3lBdyJ9.azWklrByWOL7jmYb0KSRdQ';
 
-var map = L.mapbox.map('map', 'alexandergugel.k21hb9dm');
+var InteractiveVenueMap = function (id, options) {
+  id = id || 'map';
+  options = options || 'alexandergugel.k21hb9dm';
 
-// map.setView([40, -74.50], 9);
+  this._initMap(id, options);
+  this._initClusterGroup();
+};
 
-var markers = new L.MarkerClusterGroup({
-  iconCreateFunction: function (cluster) {
-    return new L.divIcon({
-      iconSize: L.point(56, (function calcHeight() {
-        var rows = cluster.getChildCount();
-        if (rows % 2 !== 0) {
-          rows++;
-        }
-        rows = rows/2;
-        return rows*26 + 4;
-      })()),
-      className: 'venue-marker',
-      html: (function () {
-        var html = '';
-        for (var i = 0; i < cluster.getChildCount(); i++) {
-          html += '<div class="venue"></div>';
-        }
-        return html;
-      })()
-    });
-  }
-});
+InteractiveVenueMap.prototype._initMap = function (id, options) {
+  this.map = L.mapbox.map(id, options);
+};
 
-for (var i = 0; i < 100; i++) {
-  var marker = L.marker(new L.LatLng(i, i), {
-      icon: L.divIcon({
-        iconSize: L.point(30, 30),
+InteractiveVenueMap.prototype._initClusterGroup = function () {
+  this.venueClusterGroup = new L.MarkerClusterGroup({
+    iconCreateFunction: function (cluster) {
+      return new L.divIcon({
+        iconSize: L.point(56, (function calcHeight() {
+          var rows = cluster.getChildCount();
+          if (rows % 2 !== 0) {
+            rows++;
+          }
+          rows = rows/2;
+          return rows*26 + 4;
+        })()),
         className: 'venue-marker',
-        html: '<div class="venue"></div>'
-      }),
-      title: i
+        html: (function () {
+          var html = '';
+          for (var i = 0; i < cluster.getChildCount(); i++) {
+            html += '<div class="venue"></div>';
+          }
+          return html;
+        })()
+      });
+    }
   });
-  marker.bindPopup(i);
-  markers.addLayer(marker);
-}
 
-map.addLayer(markers);
+  this.map.addLayer(this.venueClusterGroup);
+};
+
+InteractiveVenueMap.prototype.renderVenues = function (venues) {
+  for (var i = 0; i < 100; i++) {
+    var marker = L.marker(new L.LatLng(i, i), {
+        icon: L.divIcon({
+          iconSize: L.point(30, 30),
+          className: 'venue-marker',
+          html: '<div class="venue"></div>'
+        }),
+        title: i
+    });
+    marker.bindPopup(i);
+    this.venueClusterGroup.addLayer(marker);
+  }
+};
+
+InteractiveVenueMap.prototype.clearVenues = function () {
+  InteractiveVenueMap.clearLayers();
+};
+
+
+var interactiveVenueMap = new InteractiveVenueMap();
+interactiveVenueMap.renderVenues();
